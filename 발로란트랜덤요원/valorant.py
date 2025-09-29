93ec39373219eb6c4de3bccdac370aa8f1c4fded
EUNGWANCHOI <silvergwan@gmail.com>
2025-09-29 16:54:39 +0900

    study :: Valorant randome agent choice.

---
+++ 발로란트랜덤요원/valorant.py
@@ -0,0 +1,11 @@
+import random
+
+agents = [
+    "Brimstone", "Viper", "Omen", "Killjoy", "Cypher",
+    "Sova", "Sage", "Phoenix", "Jett", "Reyna",
+    "Raze", "Breach", "Skye", "Yoru", "Astra",
+    "KAY/O", "Chamber", "Neon", "Fade", "Harbor",
+    "Gekko", "Deadlock", "Iso", "Clove", "Tejo", "Waylay", "Vyse"
+]
+
+print(random.choice(agents))
